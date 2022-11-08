29124557f3c1107861908339b68edd06793410f4
FinnTVD <trinhvanduc21062001@gmail.com>
2022-11-08 14:38:25 +0700

    loading 99%

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,25 @@ window.addEventListener("scroll", (e) => {
 		btnTop.style.display = "none";
 	}
 });
+const search = document.querySelector(".navbar--search");
+const popUpSearch = document.querySelector(".popup__search");
+const btnCloseSearch = document.querySelector(".popup__search--close");
+const btnCart = document.querySelector(".btn--cart");
+const popUpCart = document.querySelector(".popup__cart");
+const btnCloseCart = document.querySelector(".popup__cart--close");
+search.addEventListener("click", () => {
+	popUpSearch.style.display = "block";
+	popUpCart.style.display = "none";
+});
+btnCloseSearch.addEventListener("click", () => {
+	popUpSearch.style.display = "none";
+});
+
+btnCart.addEventListener("click", () => {
+	popUpCart.style.display = "block";
+	btnCart.style.border = "2px solid #fff";
+});
+btnCloseCart.addEventListener("click", () => {
+	popUpCart.style.display = "none";
+	btnCart.style.border = "2px solid #000";
+});
